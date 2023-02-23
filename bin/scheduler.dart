2211a01c03886797f63ad827bcd524362e4146a0
Anvar <anvarkangadiyil@gmail.com>
2023-02-23 15:06:55 +0530

    final version

--- bin/scheduler.dart
+++ bin/scheduler.dart
@@ -2,21 +2,21 @@ import 'dart:async';
 import 'dart:io';
 
 void main() async {
-  await task(hours: 0, minutes: 0, seconds: 13, discreption: "display");
+  final stopwatch = Stopwatch()..start();
+  await Schedule(hours: 0, minutes: 0, seconds: 10, discreption: "display");
+  stopwatch.stop();
+  print('Time taken to execute method: ${stopwatch.elapsed.inMilliseconds}');
 }
 
-Future<void> task({
-  required hours,
-  required minutes,
-  required seconds,
-  String? discreption,
-}) async {
+Future<void> Schedule(
+    {required hours,
+    required minutes,
+    required seconds,
+    String? discreption}) async {
   final timeNow = DateTime.now();
-  print("the tasks is initiated at time :: $timeNow");
+  print("the tasks is initiated at time :: $timeNow\n");
   await Future.delayed(
     Duration(hours: hours, minutes: minutes, seconds: seconds),
   );
   print('executing task named $discreption ....\n');
-  var timeTaken = DateTime.now();
-  print('task is finished at time :: $timeTaken\n');
 }
